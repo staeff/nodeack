4ac96cacf3f000d12769d12fed444ee271b84122
Stephan Klinger <stephan.klinger@freitag.de>
2016-08-29 16:02:28 +0200

    Update formatting to new liniting rules

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 #!/usr/bin/env node
 
-var tags = require("./src/tags.js");
-var search = require("./src/search.js");
+var tags = require('./src/tags.js');
+var search = require('./src/search.js');
 
 var defaults = {
-    path: ".",
-    query: "",
+    path: '.',
+    query: '',
     depth: 2
 }
 var replacements = {
-    p: "path",
-    q: "query",
-    d: "depth",
-    h: "help"
+    p: 'path',
+    q: 'query',
+    d: 'depth',
+    h: 'help'
 }
 
 tags = tags.parse(process.argv, defaults, replacements);
 
 if(tags.help) {
-    console.log("Usage: ./app.js -q=query [-d=depth] [-p=path]");
+    console.log('Usage: ./app.js -q=query [-d=depth] [-p=path]');
 } else {
     search.scan(tags.path, tags.depth, function(err, files){
         search.match(tags.query, files).forEach(function(file){
--- test/tagsSpec.js
+++ test/tagsSpec.js
@@ -1,50 +1,53 @@
-var expect = require("chai").expect;
-var tags = require("../src/tags.js");
+var expect = require('chai').expect;
+var tags = require('../src/tags.js');
 
-describe("Tags", function(){
-    describe("#parse()", function(){
-        it("should accept key-value options and parse them into an object", function(){
-            var args = ["--depth=4", "--hello=world"];
-            var results = tags.parse(args);
+describe('Tags', function() {
+  describe('#parse()', function() {
+    it('should accept key-value options and parse them into an object', function() {
+      var args = ['--depth=4', '--hello=world'];
+      var results = tags.parse(args);
 
-            expect(results).to.have.a.property("depth", 4);
-            expect(results).to.have.a.property("hello", "world");
-        });
-        it("should fallback to defaults", function(){
-            var args = ["--depth=4", "--hello=world"];
-            var defaults = {depth: 2, foo: "bar"};
-            var results = tags.parse(args, defaults);
-
-            var expected = {
-                depth: 4,
-                foo: "bar",
-                hello: "world"
-            };
-            expect(results).to.deep.equal(expected);
-        });
-        it("should accept simple options and set them", function(){
-            var args = ["--searchContents"];
-            var results = tags.parse(args);
-
-            expect(results).to.have.a.property("searchContents", true);
-        });
-        it("should accept short options",function(){
-            var args = ["-sd=4", "-h"];
-            var replacements = {
-                s: "searchContents",
-                d: "depth",
-                h: "hello"
-            };
-
-            var results = tags.parse(args, {}, replacements);
-
-            var expected = {
-                searchContents: true,
-                depth: 4,
-                hello: true
-            };
-
-            expect(results).to.deep.equal(expected);
-        });
+      expect(results).to.have.a.property('depth', 4);
+      expect(results).to.have.a.property('hello', 'world');
     });
+    it('should fallback to defaults', function() {
+      var args = ['--depth=4', '--hello=world'];
+      var defaults = {
+        depth: 2,
+        foo: 'bar'
+      };
+      var results = tags.parse(args, defaults);
+
+      var expected = {
+        depth: 4,
+        foo: 'bar',
+        hello: 'world'
+      };
+      expect(results).to.deep.equal(expected);
+    });
+    it('should accept simple options and set them', function() {
+      var args = ['--searchContents'];
+      var results = tags.parse(args);
+
+      expect(results).to.have.a.property('searchContents', true);
+    });
+    it('should accept short options', function() {
+      var args = ['-sd=4', '-h'];
+      var replacements = {
+        s: 'searchContents',
+        d: 'depth',
+        h: 'hello'
+      };
+
+      var results = tags.parse(args, {}, replacements);
+
+      var expected = {
+        searchContents: true,
+        depth: 4,
+        hello: true
+      };
+
+      expect(results).to.deep.equal(expected);
+    });
+  });
 });
